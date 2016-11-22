5a073cefa5779685182e24adcfbe813261ce3d8b
chivalry02 <chivalry02@163.com>
2016-11-22 09:07:56 +0800

    commit

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 
 require('http').createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.end('OK')
+	
+	var tmp = request.headers['accept-language'].split(/[,;]/);
+	var language = tmp[0];
+	
+	var tmp = request.headers['user-agent'].match(/.+?(\(.+?\)).+/);
+	var software = tmp[1];
+	software = software.slice(1, software.length-1);
+	
+	var out = {
+		'ipaddress':request.headers.host,
+		'language':language,
+		'software':software
+	};
+	response.end(JSON.stringify(out));
 }).listen(process.env.PORT || 5000);
+
+
 
